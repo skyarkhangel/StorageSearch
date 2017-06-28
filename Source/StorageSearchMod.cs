3bc48ab6605d94d6702f189a31c19b8aeedd36a1
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2017-06-28 16:19:49 +0200

    Switched detours to Harmony (https://github.com/pardeike/Harmony)

---
+++ Source/StorageSearchMod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Harmony;
+using Verse;
+
+namespace StorageSearch
+{
+    public class StorageSearchMod : Mod
+    {
+        public StorageSearchMod(ModContentPack content) : base(content)
+        {
+
+        }
+
+        [StaticConstructorOnStartup]
+        public class Injector {
+            static Injector()
+            {
+                var harmonyInstance = HarmonyInstance.Create("StorageSearch");
+                harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());              // just use all [HarmonyPatch] decorated classes                
+            }
+        }
+    }
+}
