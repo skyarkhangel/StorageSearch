faa10a34d1d5a688f2d10eb9616e16efa4808fc7
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2017-06-30 17:48:11 +0200

    Added search to crafting filter (still needs proper positioning of edit box)

---
+++ Source/Patches/Bills/Dialog_BillConfig_DoWindowContents.cs
@@ -0,0 +1,14 @@
+﻿using Harmony;
+using RimWorld;
+
+namespace StorageSearch {
+    [HarmonyPatch(typeof(Dialog_BillConfig), nameof(Dialog_BillConfig.DoWindowContents))]
+    class Dialog_BillConfig_DoWindowContents
+    {
+
+        [HarmonyPrefix]
+        public static void Before_DoWindowContents() {
+            ThingFilterUI_DoThingFilterConfigWindow.showSearchCount++;
+        }
+    }
+}
