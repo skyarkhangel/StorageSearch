def78fbb03b42b21bdc3b78527e3556f9ab63b22
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2017-07-01 02:26:32 +0200

    Special case (and modify) ExtendedCrafting UI

--- Source/StorageSearchMod.cs
+++ Source/StorageSearchMod.cs
@@ -26,13 +26,27 @@ namespace StorageSearch
 
 
         [StaticConstructorOnStartup]
-        public class Injector {
+        public class Injector
+        {
             static Injector()
             {
                 HarmonyInstance harmonyInstance = HarmonyInstance.Create("StorageSearch");
                 harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());              // just use all [HarmonyPatch] decorated classes       
-                
-                Log.Message("StorageSearch injected...");         
+
+                bool modifiedExtendedCrafting = TryDetourExtendedCrafting(harmonyInstance);               
+
+                Log.Message($"StorageSearch injected {(modifiedExtendedCrafting ? "(ExtendedCrafting detected)" : String.Empty)} ...");         
+            }
+
+            private static bool TryDetourExtendedCrafting(HarmonyInstance harmony) {
+                var ecAssembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == "AcEnhancedCrafting");
+                if (ecAssembly != null)
+                {
+                    MethodInfo mi = ecAssembly.GetType("AlcoholV.Overriding.Dialog_BillConfig").GetMethod(nameof(RimWorld.Dialog_BillConfig.DoWindowContents));
+                    harmony.Patch(mi, new HarmonyMethod(typeof(Dialog_BillConfig_DoWindowContents), nameof(Dialog_BillConfig_DoWindowContents.Before_DoWindowContents)), null, null);
+                    return true;
+                }
+                return false;
             }
         }
     }
